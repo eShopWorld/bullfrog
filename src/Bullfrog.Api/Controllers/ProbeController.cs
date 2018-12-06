de110f799a78de5023a86dbfae9b2bedef87250c
Artur Zgodzinski <azgodzinski@eshopworld.com>
2018-12-06 15:51:07 +0000

    Fixed SF configuration

---
+++ src/Bullfrog.Api/Controllers/ProbeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bullfrog.Api.Controllers
+{
+    /// <summary>
+    /// probe controller
+    /// </summary>
+    [Route("/probe")]
+    [AllowAnonymous]
+    public class ProbeController : Controller
+    {
+        /// <summary>
+        /// probe endpoint
+        /// </summary>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
