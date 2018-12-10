9d8824f4164f6841af8a3673fb19f5cd45fb3106
Artur Zgodzinski <azgodzinski@eshopworld.com>
2018-12-10 09:51:04 +0000

    using environment enum instead of strirng comparasions

--- src/Bullfrog.Api/EswSslExtentions.cs
+++ src/Bullfrog.Api/EswSslExtentions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Eshopworld.DevOps;
 using Eshopworld.Telemetry;
 using Microsoft.AspNetCore.Hosting;
 
@@ -17,7 +18,11 @@ namespace Bullfrog.Api
              {
                  options.Listen(IPAddress.Any, port, listenOptions =>
                  {
-                     var environment = context.HostingEnvironment.EnvironmentName;
+                     var environmentName = context.HostingEnvironment.EnvironmentName;
+                     if (!Enum.TryParse<DeploymentEnvironment>(environmentName, true, out var environment))
+                     {
+                         environment = DeploymentEnvironment.Development;   // TODO: other default or exception? BB?
+                     }
                      var cert = GetCertificate(environment);
                      listenOptions.UseHttps(cert);
                      listenOptions.NoDelay = true;
@@ -25,18 +30,18 @@ namespace Bullfrog.Api
              });
         }
 
-        private static X509Certificate2 GetCertificate(string environment)
+        private static X509Certificate2 GetCertificate(DeploymentEnvironment environment)
         {
-            var tld = "PROD".Equals(environment, StringComparison.OrdinalIgnoreCase) ? "com" : "net";
+            var tld = environment == DeploymentEnvironment.Prod ? "com" : "net";
             var subject = $"star.{environment}.eshopworld.{tld}";
             var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
             try
             {
                 store.Open(OpenFlags.ReadOnly);
-                var certCollection = store.Certificates.Find(X509FindType.FindBySubjectDistinguishedName,$"CN={subject}", false);
+                var certCollection = store.Certificates.Find(X509FindType.FindBySubjectDistinguishedName, $"CN={subject}", false);
                 if (certCollection.Count == 0)
                 {
-                    if("DEVELOPMENT".Equals(environment, StringComparison.OrdinalIgnoreCase))
+                    if (environment == DeploymentEnvironment.Development)
                     {
                         var devCerts = store.Certificates.Find(X509FindType.FindBySubjectDistinguishedName, "CN=localhost", false);
                         if (devCerts.Count > 0)
