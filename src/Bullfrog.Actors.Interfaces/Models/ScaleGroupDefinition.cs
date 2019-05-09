3bbe6ea29f0f5c80f60f8ecd4e7e1ae1d5b85206
Artur Zgodzinski <azgodzinski@eshopworld.com>
2019-05-09 14:09:50 +0100

    Improved the handling of the special Cosmos region.

--- src/Bullfrog.Actors.Interfaces/Models/ScaleGroupDefinition.cs
+++ src/Bullfrog.Actors.Interfaces/Models/ScaleGroupDefinition.cs
@@ -12,14 +12,34 @@ namespace Bullfrog.Actors.Interfaces.Models
     /// </summary>
     public class ScaleGroupDefinition
     {
+        /// <summary>
+        /// The name of an special region which handles shared Cosmos databases.
+        /// </summary>
+        public const string SharedCosmosRegion = "$cosmos";
+
         private Dictionary<string, ScaleGroupRegion> _regionsByName;
 
-        internal ScaleGroupRegion this[string regionName]
+        /// <summary>
+        /// Regions (including the special Cosmos region) indexed by name.
+        /// </summary>
+        /// <param name="regionName">The region name.</param>
+        /// <returns>The region definition.</returns>
+        public ScaleGroupRegion this[string regionName]
         {
             get
             {
                 if (_regionsByName == null)
                     _regionsByName = Regions.ToDictionary(r => r.RegionName);
+                if (HasSharedCosmosDb && !_regionsByName.ContainsKey(SharedCosmosRegion))
+                {
+                    _regionsByName.Add(SharedCosmosRegion, new ScaleGroupRegion
+                    {
+                        RegionName = SharedCosmosRegion,
+                        CosmosDbPrescaleLeadTime = CosmosDbPrescaleLeadTime,
+                        Cosmos = Cosmos,
+                        ScaleSets = new List<ScaleSetConfiguration>(),
+                    });
+                }
 
                 return _regionsByName[regionName];
             }
