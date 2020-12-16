1740af28484c5897e1354ec16824f2aea6603bfa
Nikhil Khandelwal <nkhandelwal@eshopworld.com>
2020-12-16 18:54:51 +0000

    Excluded 5 files from coverage

--- src/Bullfrog.Actors/EventModels/ResourceScalingIssue.cs
+++ src/Bullfrog.Actors/EventModels/ResourceScalingIssue.cs
@@ -1,7 +1,9 @@
-﻿using Eshopworld.Core;
+﻿using System.Diagnostics.CodeAnalysis;
+using Eshopworld.Core;
 
 namespace Bullfrog.Actors.EventModels
 {
+    [ExcludeFromCodeCoverage]
     public class ResourceScalingIssue : TelemetryEvent
     {
         public string ResourceName { get; set; }
--- src/Bullfrog.Actors/Models/ScaleSetCurrentState.cs
+++ src/Bullfrog.Actors/Models/ScaleSetCurrentState.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace Bullfrog.Actors.Models
 {
+    [ExcludeFromCodeCoverage]
     public class ScaleSetCurrentState
     {
         /// <summary>
